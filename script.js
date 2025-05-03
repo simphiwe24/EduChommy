3b557d2677a5e259e15b476a4c7ce1fe8defbc14
Simphiwe Sinenhlanhla  Mdokha <158762074+simphiwe24@users.noreply.github.com>
2025-05-03 19:29:57 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "AIzaSyChSWxGCh2WzEqHFDawqiW0SIbuD_PBepo";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+const API_KEY = "AIzaSyCAefT6m_2sM3_u1OM6cOaQ-fyTrjRu97M";
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+
 
 const userData = {
   message: null,
