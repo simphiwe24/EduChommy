d83bf9318e8669253c3f8fce0af4f0c6b0bd0410
noluMolui <noluthandotinyiko@gmail.com>
2025-05-04 02:30:12 +0200

    linked the pages together

--- navBar.js
+++ navBar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 fetch('navBar.html')
@@ -15,21 +14,3 @@ fetch('navBar.html')
   });
 });
 
-=======
-
-
-fetch('navBar.html')
-.then(res => res.text())
-.then(data => {
-  document.getElementById('navbar').innerHTML = data;
-
-  
-  const toggleBtn = document.querySelector('.menu-toggle');
-  const navLinks = document.querySelector('.nav-links');
-
-  toggleBtn?.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-  });
-});
-
->>>>>>> 578132c90620872fcf87bab4a23c5d18c3cad284
